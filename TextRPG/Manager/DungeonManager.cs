98acc22ab380eec5855c05c7f0caf870b19d3d25
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-07-11 14:29:50 +0900

    던전 추가

--- TextRPG/Manager/DungeonManager.cs
+++ TextRPG/Manager/DungeonManager.cs
@@ -29,15 +29,13 @@ namespace TextRPG.Manager
             Dungeons = new List<Dungeon>();
             InitializeDungeons();
         }
-
         private void InitializeDungeons()
         {
-            // 기본 던전 추가
-            Dungeons.Add(new Dungeon("기본 던전", "초보자를 위한 기본 던전입니다.", 1, 1000, new List<string> { "슬라임", "고블린" }, 5));
-            Dungeons.Add(new Dungeon("어두운 동굴", "어두운 동굴로, 강력한 몬스터가 출몰합니다.", 2, 1700, new List<string> { "박쥐", "늑대" }, 10));
-            Dungeons.Add(new Dungeon("고대 유적", "고대의 유적지로, 강력한 보스가 존재합니다.", 3, 2500, new List<string> { "좀비", "스켈레톤" }, 15));
-            Dungeons.Add(new Dungeon("용의 둥지", "용이 서식하는 위험한 지역입니다.", 4, 5000, new List<string> { "용", "드래곤" }, 20));
-            Dungeons.Add(new Dungeon("마법사의 탑", "강력한 마법사가 지배하는 탑입니다.", 5, 7000, new List<string> { "마법사", "고블린 마법사" }, 25));
+            Dungeons.Add(new Dungeon("초원지대", "초보 모험가들이 첫발을 내딛는 평화로운 초원입니다.", 1, 10, new List<string> { "슬라임", "토끼", "벌레" }, 5));
+            Dungeons.Add(new Dungeon("고목숲", "무성한 나무가 우거진 숲으로 중간 난이도의 몬스터가 서식합니다.", 5, 30, new List<string> { "고블린", "늑대", "늑대인간" }, 12));
+            Dungeons.Add(new Dungeon("붉은협곡", "화산 지대가 펼쳐진 붉은 협곡, 강력한 몬스터들이 출몰합니다.", 10, 90, new List<string> { "용암거북이", "화염정령", "화염드래곤" }, 18));
+            Dungeons.Add(new Dungeon("고대유적", "잊혀진 고대의 유적, 강력한 보스와 몬스터가 기다립니다.", 15, 200, new List<string> { "스켈레톤전사", "좀비마법사", "대장스켈레톤" }, 25));
+            Dungeons.Add(new Dungeon("심연의탑", "최종 보스가 숨어있는 위험한 심연의 탑입니다.", 20, 400, new List<string> { "심연의군주", "다크엘프", "고대용" }, 35));
         }
     }
 }
